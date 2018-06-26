1abdd41578203513d576261a07b30852766eafcd
Alejandro R <Alexrom789@Gmail.com>
2018-06-26 16:44:33 -0400

    started cleaning hook files

--- src/components/D3Tree.js
+++ src/components/D3Tree.js
@@ -4,13 +4,14 @@ import Tree from 'react-d3-tree';
 class D3Tree extends Component {
   constructor(props) {
     super(props);
+    Tree.defaultProps.orientation = 'vertical'
 
   }
   
   render() {
     console.log('Data: ', this.props.treeData);
     return (
-      <div id="treeWrapper" style={{width: '98%', height: '98%'}}>
+      <div id="treeWrapper" style={{width: '98%', height: '95%'}}>
         {this.props.treeData && <Tree data={this.props.treeData} /> }
       </div>
     );
