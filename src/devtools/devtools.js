0ba9a1554ad3b6db36643c5ee687f49b8050bc56
Alejandro R <Alexrom789@Gmail.com>
2018-07-02 14:51:14 -0400

    added new log window to bot

--- src/devtools/devtools.js
+++ src/devtools/devtools.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import D3Tree from '../components/D3Tree';
 import InfoWindow from '../components/InfoWindow';
+import LogWindow from '../components/LogWindow';
 import '../styles/App.css';
 import NavBar from '../components/NavBar';
 import ChartWindow from '../components/ChartWindow';
@@ -255,7 +256,7 @@ class App extends Component {
 
     return (
       
-      <div className='test'>
+      <div className='appWindow'>
         <NavBar/>
         <button className="button" onClick={()=>this.handleClick('dom')}>DOMs</button>
         <span> </span>
@@ -265,10 +266,13 @@ class App extends Component {
         <span> </span>
         <button className="button" onClick={this.handleClick.bind(this, 'props')}>Show Props</button>
         <div className="rowCols">
-        <ChartWindow treeType='Components:' treeData={this.state.data} onMouseOver={this.onMouseOver}/>
-        <ChartWindow treeType='Store:' storeData={this.state.storeHistory} onMouseOver={this.onMouseOver}/>
+          <ChartWindow treeType='Components:' treeData={this.state.data} onMouseOver={this.onMouseOver}/>
+          <ChartWindow treeType='Store:' storeData={this.state.storeHistory} onMouseOver={this.onMouseOver}/>
         </div>
+        <div className="rowCols">
         <InfoWindow propsData={this.state.props}/>
+        <LogWindow/>
+        </div>
         <br />
       </div>
     );
